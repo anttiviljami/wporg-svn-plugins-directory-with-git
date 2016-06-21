7a95c07c85100da75f5ea873a49fd1b0184b306e
Antti Kuosmanen <antti@seravo.fi>
2016-06-21 12:49:17 +0300

    Add nice error messages for exceptions

--- src/main.rb
+++ src/main.rb
@@ -4,8 +4,14 @@
 
 require 'yaml'
 require 'shellwords'
+require 'colorize'
 
-plugins = YAML.load_file(File.join(BASEDIR, 'config.yml'))['plugins']
+unless File.exist? File.join(BASEDIR, 'config.yml')
+  puts "Error:".red + " config.yml seems to be missing."
+  exit 1
+else
+  plugins = YAML.load_file(File.join(BASEDIR, 'config.yml'))['plugins']
+end
 
 plugins.each do |plugin|
   name = plugin[0]
@@ -19,10 +25,15 @@ plugins.each do |plugin|
 
   if Dir.exist? gitdir
     puts "Pulling git repository for #{name} at #{gitdir}..."
-    system "cd #{gitdir.shellescape} && git pull origin"
+    result = system "cd #{gitdir.shellescape} && git pull #{gitsrc}"
   else
     puts "Cloning git repository for #{name} to #{gitdir}..."
-    system "git clone #{gitsrc} #{gitdir.shellescape}"
+    result = system "git clone #{gitsrc} #{gitdir.shellescape}"
+  end
+
+  unless result
+    puts "Warning:".yellow + " Couldn't fetch git repo for #{name}, skipping updates..."
+    next
   end
 
   if Dir.exist? svndir
@@ -31,6 +42,11 @@ plugins.each do |plugin|
   else
     puts "Checking out svn repository for #{name} to #{svndir}..."
     system "svn co #{svnsrc} #{svndir.shellescape}"
+  end
+
+  unless result
+    puts "Warning:".yellow + " Couldn't fetch svn repo for #{name}, skipping updates..."
+    next
   end
 end
 
